f291d7eaaff37c8c07b98ca15acf0c2a073b2c64
Peter Williams <pezra@barelyenough.org>
2010-09-07 21:07:50 -0600

    added before spec hook to sandbox test redis data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,11 @@ require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+  config.before(:each) do 
+    Resque.redis.namespace = "resque-multi-step-task-testing"
+    Resque.redis.keys('*').each{|k| Resque.redis.del k}
+    
+    # Tests are jailed to a testing namespace and that space does
+    # contains in left over data from previous runs.
+  end
 end
