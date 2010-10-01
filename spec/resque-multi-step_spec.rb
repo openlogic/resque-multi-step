82586db3f02c5fb749da5c32edf6467ca2c10cef
Peter Williams <pezra@barelyenough.org>
2010-09-30 21:28:43 -0600

    Added valid test to gem name test

--- spec/resque-multi-step_spec.rb
+++ spec/resque-multi-step_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ResqueMultiStepTask" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "defines MultiStepTask" do 
+    defined?(Resque::Plugins::MultiStepTask).should be_true
   end
 end
